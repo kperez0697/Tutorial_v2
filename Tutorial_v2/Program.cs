c7d33970b36394033e3609246ce6e5944f138beb
Kevin Perez Otero <kperez@abhisoft.com.pe>
2025-05-25 12:35:19 -0500

    Agregar archivos de proyecto.

---
+++ Tutorial_v2/Program.cs
@@ -0,0 +1,30 @@
+using QuestPDF.Infrastructure;
+using Tutorial_v2.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddBlazorBootstrap();
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+QuestPDF.Settings.License = LicenseType.Community;
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
+
